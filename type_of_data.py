b8f9451141fdf995258dc5f9db4a96331df3b879
shehab nasser <shehab.nasser@arab-security.com>
2025-06-21 17:30:18 +0300

    add file type of data

---
+++ type_of_data.py
@@ -0,0 +1,30 @@
+# type () bulit in funcation to determine type of data
+
+# string type
+
+print("shehab nasser") # string << string 
+
+
+# interger type << number
+
+print(10) # int << interger
+
+# floating point number
+
+print(10.90) # float << floating point number
+
+# boalen number
+
+print(10 == 12) #  bool << boalean number
+
+# list type 
+
+print([1,2,3,4,5])  # list << list
+
+# tuple 
+print((1,2,3,4,5)) # tuple 
+
+# dictionary 
+
+print({"key": " value"})   # dict << dictionary
+
